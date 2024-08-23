b83a0dc2123a9563d7a8145391878d5885086c34
Arnav Gilankar <86144763+Arnav9923386924@users.noreply.github.com>
2024-08-23 21:33:05 +0530

    Update twitter_trial.sql

--- core/classes/twitter_trial.sql
+++ core/classes/twitter_trial.sql
@@ -3,6 +3,6 @@ create database twitter_trail;
 use twitter_trail;
 
 --import twitter.sql 
-source twitter_core/classes/twitter_trial.sql;
+source twitter_clone/core/classes/twitter_trial.sql;
 
 exit;
